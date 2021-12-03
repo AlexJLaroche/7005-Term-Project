5708d7544c47cf86d596cb3dc3458b53688374d3
Kalen R Tara <kalentara@gmail.com>
2021-12-03 02:12:03 -0800

    updated captures
    big captures removed, filtered ones remained

--- Configuration/Transmitter/transmit_config.py
+++ Configuration/Transmitter/transmit_config.py
@@ -8,6 +8,6 @@ receive_address = '192.168.1.200'
 receive_port = 7005
 
 total_packet = 30
-window_size = 4 # must be values like 1,2,4,8,16.....
+window_size = 8 # must be values like 1,2,4,8,16.....
 timeout = 10
 max_packet_size = 4
