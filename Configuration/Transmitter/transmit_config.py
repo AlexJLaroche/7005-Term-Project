bf9a251c3bc72f4c46c19b58ae84fb326760b314
Kalen R Tara <kalentara@gmail.com>
2021-12-02 18:02:50 -0800

    Cats
    Cats are in fact not real

---
+++ Configuration/Transmitter/transmit_config.py
@@ -0,0 +1,13 @@
+net_address = '192.168.1.91'
+net_port = 7005
+
+transmit_address = '192.168.1.88'
+transmit_port = 7005
+
+receive_address = '192.168.1.200'
+receive_port = 7005
+
+total_packet = 30
+window_size = 4 # must be values like 1,2,4,8,16.....
+timeout = 10
+max_packet_size = 4
